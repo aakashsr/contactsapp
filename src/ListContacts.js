51b8069b8a8956a82c26866525d63666dc2f3bdc
aakashsr <aakashsri.dev@gmail.com>
2019-08-08 12:49:31 +0530

    applied map() method on contacts array to show each name of each list item individually

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 class ListContacts extends Component {
+
     render() {
+        const contacts = this.props.contacts ;
+        console.log(contacts);
         return (
            <ol className='contact-list'>
-               
+            {contacts.map( (contact) => {
+                return <li key={contact.name}>{contact.name}</li>
+            })}
            </ol>
         )
     }
