ce7734c08e83f2d846a5b65a4f2e98e56b4fd589
aakashsr <aakashsri.dev@gmail.com>
2019-08-08 17:01:35 +0530

    added a search field using a controlled component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,32 +2,50 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class ListContacts extends Component {
-  // const { removeContacts } = removeContacts ;
+  state = {
+    query: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      query: event.target.value
+    });
+  };
   render() {
-    // const contacts = this.props.contacts;
     return (
-      <ol className="contact-list">
-        {this.props.contacts.map(contact => (
-          <li key={contact.id} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}
-            />
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.handle}</p>
-            </div>
-            <button
-              onClick={() => this.props.onDeleteContact(contact)} // One thing to note is that since we need to pass in contact as an argument we need to wrap the onDeleteContact function call in an arrow function.
-              className="contact-remove"
-            >
-              Remove
-            </button>
-          </li>
-        ))}
-      </ol>
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search Contacts"
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+        </div>
+        <ol className="contact-list">
+          {this.props.contacts.map(contact => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }}
+              />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <button
+                onClick={() => this.props.onDeleteContact(contact)} // One thing to note is that since we need to pass in contact as an argument we need to wrap the onDeleteContact function call in an arrow function.
+                className="contact-remove"
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
     );
   }
 }
